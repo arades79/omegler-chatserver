4e29ba2045d13a3370c5dfdc922faed7e0652b90
arades79 <scravers@protonmail.com>
2021-05-09 10:31:14 -0400

    remove commented code using fancy data structures

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,10 @@ use std::sync::{Mutex, RwLock};
 
 const MAX_PENDING_MESSAGES: usize = 16;
 
-// type RecieveChannel = channel::Receiver<Vec<u8>>;
-// type SendChannel = channel::Sender<Vec<u8>>;
-// type SendMapWriteHandle = evmap::WriteHandle<[u8; 32], SendChannel>;
-// type RecvMapWriteHandle = evmap::WriteHandle<[u8; 32], RecieveChannel>;
-// type SendMapReadHandle = evmap::ReadHandle<[u8; 32], SendChannel>;
-// type RecvMapReadHandle = evmap::ReadHandle<[u8; 32], RecieveChannel>;
-
 type MessageQueueMap = RwLock<HashMap<PubKey, Mutex<Vec<Message>>>>;
 type PubKey = [u8; 32];
 type Message = Vec<u8>;
 type InactiveUserSet = Mutex<HashSet<PubKey>>;
-
-// fn pair_users(
-//     user_send_map: Mutex<SendMapWriteHandle>,
-//     user_recv_map: Mutex<RecvMapWriteHandle>,
-//     sender_pub_key: [u8; 32],
-//     reciever_pub_key: [u8; 32],
-// ) {
-//     let (message_send_handle, message_recieve_handle) = channel::bounded(MAX_PENDING_MESSAGES);
-//     let (other_user_send_handle, other_user_recieve_handle) =
-//         channel::bounded(MAX_PENDING_MESSAGES);
-//     if let Ok(lock) = user_send_map.lock() {
-//         lock.insert(sender_pub_key, message_send_handle);
-//         lock.insert(reciever_pub_key, other_user_send_handle);
-//     }
-//     if let Ok(lock) = user_recv_map.lock() {
-//         lock.insert(reciever_pub_key, message_recieve_handle);
-//         lock.insert(sender_pub_key, other_user_recieve_handle);
-//     }
-// }
 
 fn sign_in(inactive_users: InactiveUserSet, pub_key: PubKey) {
     if let Ok(mut users) = inactive_users.lock() {
