4fdc7639e6d4ce2f92e7d19f10ac2af19d505802
arades79 <scravers@protonmail.com>
2021-05-10 00:35:57 -0400

    make functions into actix API methods

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 #![allow(dead_code)]
 
+use actix_web::{
+    delete, get, post,
+    web::{Bytes, Data, Path, Query},
+    App, HttpResponse, HttpServer, Responder,
+};
+#[allow(unused_imports)] // needs to be imported to give method access
 use rand::{seq::IteratorRandom, thread_rng};
+
+use serde_derive::Deserialize;
 use std::collections::{HashMap, HashSet};
 use tokio::sync::{Mutex, RwLock};
 
-const MAX_PENDING_MESSAGES: usize = 16;
+type MessageQueueMap = RwLock<HashMap<String, Mutex<Vec<Message>>>>;
+type Message = Bytes;
+type InactiveUserSet = Mutex<HashSet<String>>;
 
-type MessageQueueMap = RwLock<HashMap<PubKey, Mutex<Vec<Message>>>>;
-type PubKey = [u8; 32];
-type Message = Vec<u8>;
-type InactiveUserSet = Mutex<HashSet<PubKey>>;
-
-async fn sign_in(inactive_users: InactiveUserSet, pub_key: PubKey) {
-    if let Ok(mut users) = inactive_users.lock().await {
-        users.insert(pub_key);
-    }
+#[derive(Debug, Deserialize)]
+struct PubKeyPair {
+    pub_key: String,
+    other_pub_key: String,
 }
 
-async fn get_chat_partner(inactive_users: InactiveUserSet, pub_key: PubKey) -> Option<PubKey> {
-    if let Ok(mut users) = inactive_users.lock().await {
-        let partner = { *users.iter().choose(&mut thread_rng())? };
+#[post("/users")]
+async fn sign_in(inactive_users: Data<InactiveUserSet>, pub_key: String) -> impl Responder {
+    let mut users = inactive_users.lock().await;
+    users.insert(pub_key);
+
+    HttpResponse::Ok()
+}
+
+#[get("/users")]
+async fn get_chat_partner(
+    inactive_users: Data<InactiveUserSet>,
+    pub_key: String,
+) -> impl Responder {
+    if let mut users = inactive_users.lock().await {
+        let partner = { users.iter().choose(&mut thread_rng())?.clone() };
         users.remove(&partner);
         users.remove(&pub_key);
         Some(partner)
@@ -28,50 +45,80 @@ async fn get_chat_partner(inactive_users: InactiveUserSet, pub_key: PubKey) -> O
     }
 }
 
+#[post("/messages")]
 async fn start_chat(
-    active_users: MessageQueueMap,
-    user_pub_key: PubKey,
-    other_user_pub_key: PubKey,
-) {
-    if let Ok(mut editable_user_map) = active_users.write().await {
-        editable_user_map.insert(user_pub_key, Mutex::new(Vec::new()));
-        editable_user_map.insert(other_user_pub_key, Mutex::new(Vec::new()));
+    active_users: Data<MessageQueueMap>,
+    pub_keys: Query<PubKeyPair>,
+) -> impl Responder {
+    let PubKeyPair {
+        pub_key,
+        other_pub_key,
+    } = pub_keys.into_inner();
+    if let mut editable_user_map = active_users.write().await {
+        editable_user_map.insert(pub_key, Mutex::new(Vec::new()));
+        editable_user_map.insert(other_pub_key, Mutex::new(Vec::new()));
     }
+
+    HttpResponse::Ok()
 }
 
-async fn send_message(active_users: MessageQueueMap, recieving_pub_key: PubKey, message: Message) {
-    if let Ok(user_map) = active_users.read().await {
-        if let Some(reciever) = user_map.get(&recieving_pub_key) {
-            if let Ok(mut send_queue) = reciever.lock().await {
-                send_queue.push(message);
-            }
-        }
+// TODO: figure out how other user gets pub key of person matched
+
+#[post("/messages/{pub_key}")]
+async fn send_message(
+    active_users: Data<MessageQueueMap>,
+    pub_key: Path<String>,
+    message: Bytes,
+) -> impl Responder {
+    let user_map = active_users.read().await;
+    if let Some(reciever) = user_map.get(pub_key.as_ref()) {
+        let mut send_queue = reciever.lock().await;
+        send_queue.push(message);
     }
+    HttpResponse::Ok()
 }
 
-async fn get_messages(active_users: MessageQueueMap, pub_key: PubKey) -> Option<Vec<Message>> {
-    if let Ok(user_map) = active_users.read().await {
-        if let Some(user_messages_mutex) = user_map.get(&pub_key) {
-            if let Ok(mut messages) = user_messages_mutex.lock().await {
-                Some(messages.drain(..).collect())
-            } else {
-                None
-            }
-        } else {
-            None
-        }
+#[get("/messages/{pub_key}")]
+async fn get_messages(
+    active_users: Data<MessageQueueMap>,
+    pub_key: Path<String>,
+) -> impl Responder {
+    let user_map = active_users.read().await;
+    if let Some(user_messages_mutex) = user_map.get(pub_key.as_ref()) {
+        let mut messages = user_messages_mutex.lock().await;
+        Some(messages.drain(..).collect::<Vec<_>>())
     } else {
         None
     }
 }
 
-async fn end_chat(active_users: MessageQueueMap, user_pub_key: PubKey, other_user_pub_key: PubKey) {
-    if let Ok(mut editable_user_map) = active_users.write().await {
-        editable_user_map.remove(&user_pub_key);
-        editable_user_map.remove(&other_user_pub_key);
-    }
+#[delete("/messages")]
+async fn end_chat(
+    active_users: Data<MessageQueueMap>,
+    pub_keys: Query<PubKeyPair>,
+) -> impl Responder {
+    let PubKeyPair {
+        pub_key,
+        other_pub_key,
+    } = pub_keys.into_inner();
+    let mut editable_user_map = active_users.write().await;
+    editable_user_map.remove(&pub_key);
+    editable_user_map.remove(&other_pub_key);
+
+    HttpResponse::Ok()
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(sign_in)
+            .service(get_chat_partner)
+            .service(start_chat)
+            .service(send_message)
+            .service(get_messages)
+    })
+    .bind("127.0.0.1:60080")?
+    .run()
+    .await
 }
